bf61c3ccbc1911427e07d051033edb839c558e2b
Alie Stone <helloaliestone@gmail.com>
2020-10-07 17:13:03 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,9 @@
 class Project
-attr_accessor :title, :backer
+attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
-    @backers = [] 
+    @backers = []
   end
 
   def add_backer(backer)
