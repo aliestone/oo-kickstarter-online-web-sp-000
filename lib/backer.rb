e7936e91a3d108c6ad58dae98a7f4804a37615b6
Alie Stone <helloaliestone@gmail.com>
2020-10-07 16:55:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,12 @@ class Backer
   @backers = []
   end
 
-  def add_backer
-    @backers << backer
+  def back_project(project)
+    @backed_projects = []
+    @backed_projects << project
   end
+  
+
 
 
 end
